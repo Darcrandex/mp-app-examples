dc52d0eeb9436144ae40a43c4439ee200e71b3e3
darcrand <darcrandex@gmail.com>
2025-10-30 08:13:08 +0800

    🐞 fix(doc): 文案修改

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,13 @@
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <h1 className='m-6 text-center text-3xl font-bold'>脑子痒工作室</h1>
+
+      <p className='m-6 indent-8'>我们是一个专注于技术服务的工作室，致力于为客户提供定制化的技术解决方案。</p>
+      <p className='m-6 indent-8'>
+        不搞死板流程，拒绝套路化输出，我们靠代码解锁创意，用 AI 玩转可能性。不管是 AI
+        工具定制、软件系统开发，还是新奇科技点子落地，只要你有需求，“脑子痒” 就敢接招，让技术服务自带潮流感与新鲜感。
+      </p>
+    </>
   )
 }
